9d0bf3423666561c0145d37ba649dbc6db077e57
Dewey Ou <oushihao97@gmail.com>
2022-03-31 09:29:26 +0800

    feat: deep readonly

--- questions/9-medium-deep-readonly/template.ts
+++ questions/9-medium-deep-readonly/template.ts
@@ -1,1 +1,8 @@
-type DeepReadonly<T> = any
+// type DeepReadonly<T> = any
+
+type DeepReadonly<T extends object> = {
+  readonly [K in keyof T]:
+  T[K] extends Record<string | number | symbol, unknown> ?
+    DeepReadonly<T[K]> :
+    T[K]
+}
