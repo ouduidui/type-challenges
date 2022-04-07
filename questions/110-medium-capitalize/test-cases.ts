b7a1b5fadfe61517d411352851f65e141b4c6570
Dewey Ou <oushihao97@gmail.com>
2022-04-07 10:15:11 +0800

    feat: MyCapitalize

--- questions/110-medium-capitalize/test-cases.ts
+++ questions/110-medium-capitalize/test-cases.ts
@@ -1,7 +1,6 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<MyCapitalize<'foobar'>, 'Foobar'>>,
   Expect<Equal<MyCapitalize<'FOOBAR'>, 'FOOBAR'>>,
   Expect<Equal<MyCapitalize<'foo bar'>, 'Foo bar'>>,
   Expect<Equal<MyCapitalize<''>, ''>>,
